4d5deaeb93d89d57d6475f3443cde667e8c86564
BraulioRuiz <ayax.720@gmail.com>
2020-06-14 03:04:05 -0500

    base de datos

---
+++ api/alumno/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path, re_path
+from django.conf.urls import include
+from django.contrib.auth.models import User
+
+from alumno import views
+
+urlpatterns = [
+    re_path(r'alumno_lista/$', views.AlumnoList.as_view()),
+    re_path(r'alumno_detalle/(?P<id>[\w\-]+)$', views.AlumnoDetail.as_view()),
+    re_path(r'asignaturas/$', views.Asignatura.as_view()),
+    re_path(r'asignaturas_detalle/(?P<id>[\w\-]+)$', views.AsignaturaDetail.as_view())
+]
---
+++ api_env_soa/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\ayax9\documents\uni\cuatri 9\soa\api_rest_soa\api_env_soa\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
